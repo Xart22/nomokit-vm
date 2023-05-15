2ff9a1a86858d3b83552f426c8de2d61d412495a
GerinAryoPrasetia <gerinaryo14@gmail.com>
2023-05-15 21:46:45 +0700

    fix url extension

--- src/extension-support/extension-manager.js
+++ src/extension-support/extension-manager.js
@@ -9,7 +9,7 @@ const maybeFormatMessage = require("../util/maybe-format-message");
 const BlockType = require("./block-type");
 
 // Local resources server address
-const localResourcesServerUrl = "https://nomopro.nomo-kit.com/";
+const localResourcesServerUrl = "https://nomobase.nomo-kit.com/";
 
 // These extensions are currently built into the VM repository but should not be loaded at startup.
 // TODO: move these out into a separate repository?
